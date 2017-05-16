096c72cb910d56d7f052e0aba3f73bc8e2685fc0
Lucile Coutouly <lucile.coutouly@obs-nancay.fr>
2017-05-16 08:04:33 +0000

    new file

--- heroic-core/src/main/java/com/spotify/heroic/http/render/RenderResource.java
+++ heroic-core/src/main/java/com/spotify/heroic/http/render/RenderResource.java
@@ -25,6 +25,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.spotify.heroic.Query;
 import com.spotify.heroic.QueryManager;
 import com.spotify.heroic.metric.QueryResult;
+import com.spotify.heroic.http.query.QueryHeatmap;
 import org.jfree.chart.JFreeChart;
 
 import javax.imageio.ImageIO;
@@ -93,6 +94,51 @@ public class RenderResource {
 
         final JFreeChart chart =
             RenderUtils.createChart(result.getGroups(), title, highlight, threshold, height);
+
+        final BufferedImage image = chart.createBufferedImage(width, height);
+
+        final ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        ImageIO.write(image, "png", buffer);
+
+        return Response.ok(buffer.toByteArray()).build();
+    }
+
+    @SuppressWarnings("unchecked")
+    @GET
+    @Path("heatmap")
+    @Produces("image/png")
+    public Response heatmap(
+        @QueryParam("q") String queryString, @QueryParam("backend") String backendGroup,
+        @QueryParam("title") String title, @QueryParam("width") Integer width,
+        @QueryParam("height") Integer height, @QueryParam("highlight") String highlightRaw,
+        @QueryParam("threshold") Double threshold
+    ) throws Exception {
+        if (query == null) {
+            throw new BadRequestException("'query' must be defined");
+        }
+
+        if (width == null) {
+            width = DEFAULT_WIDTH;
+        }
+
+        if (height == null) {
+            height = DEFAULT_HEIGHT;
+        }
+
+        final Map<String, String> highlight;
+
+        if (highlightRaw != null) {
+            highlight = mapper.readValue(highlightRaw, Map.class);
+        } else {
+            highlight = null;
+        }
+
+        final Query q = query.newQueryFromString(queryString).build();
+
+        final QueryResult result = this.query.useGroup(backendGroup).query(q).get();
+
+        final JFreeChart chart =
+            HeatmapUtil.createChart(result.getGroups(), title, highlight, threshold, height);
 
         final BufferedImage image = chart.createBufferedImage(width, height);
 
